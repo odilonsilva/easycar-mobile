f0bfba77e53be0817ac25d342f0a16f2eca07511
odilonsilva <recupera.odih@hotmail.com>
2025-02-25 18:09:59 -0300

    ajustando rotas

--- App.js
+++ App.js
@@ -1,45 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Passenger from "./src/screens/Passenger/Passenger";
-import Home from "./src/screens/Home/Home";
-import Driver from "./src/screens/Driver/Driver";
-import Details from "./src/screens/Driver/Details";
-
-const Stack = createNativeStackNavigator();
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Passenger"
-          component={Passenger}
-          options={{
-            title: "",
-            headerTransparent: true,
-            headerShadowVisible: false,
-          }}
-        />
-        <Stack.Screen
-          name="Driver"
-          component={Driver}
-          options={{ title: "Viagens disponiveis", headerStyle: "center" }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={{
-            title: "",
-            headerTransparent: true,
-            headerShadowVisible: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Routes />;
 }
---
+++ src/routes.js
@@ -0,0 +1,63 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home/Home";
+import Passenger from "./screens/Passenger/Passenger";
+import Driver from "./screens/Driver/Driver";
+import Details from "./screens/Driver/Details";
+import Login from "./screens/Login/Login";
+import { UserProvider } from "./contexts/UserContext";
+import Register from "./screens/Register/Register";
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <UserProvider>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Passenger"
+            component={Passenger}
+            options={{
+              title: "",
+              headerTransparent: true,
+              headerShadowVisible: false,
+            }}
+          />
+          <Stack.Screen
+            name="Driver"
+            component={Driver}
+            options={{
+              title: "Viagens disponiveis",
+              headerTitleAlign: "center",
+            }}
+          />
+          <Stack.Screen
+            name="Details"
+            component={Details}
+            options={{
+              title: "",
+              headerTransparent: true,
+              headerShadowVisible: false,
+            }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </UserProvider>
+    </NavigationContainer>
+  );
+}
